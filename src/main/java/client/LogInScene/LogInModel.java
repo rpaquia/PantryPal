f65cf96d879a25026efe357fa66cf8cbf04e1fc9
rpaquia <97699019+rpaquia@users.noreply.github.com>
2024-01-10 10:16:10 -0800

    Initialize MS2Demo

---
+++ src/main/java/client/LogInScene/LogInModel.java
@@ -0,0 +1,85 @@
+package client.LogInScene;
+
+import client.HttpResponse.AuthResponse;
+import client.HttpResponse.PingResponse;
+import client.HttpResponse.ServerResponse;
+import client.UserSettings;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class LogInModel {
+
+  private final UserSettings USER_SETTINGS = UserSettings.getInstance();
+  private static final String URL = "http://localhost:8100/";
+  private Boolean autoLogIn;
+
+  public ServerResponse<Boolean> pingServer() {
+    ServerResponse<Boolean> res = new PingResponse();
+    try {
+      URL url = new URI(URL + "ping/").toURL();
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+      conn.setRequestMethod("GET");
+      conn.setDoOutput(true);
+
+      int responseCode = conn.getResponseCode();
+      BufferedReader in = new BufferedReader(
+        new InputStreamReader(conn.getInputStream())
+      );
+      String response = in.readLine();
+      in.close();
+      if (responseCode == 200) {
+        res.setValidResponse("");
+      } else {
+        res.setErrorResponse(responseCode, response);
+      }
+      return res;
+    } catch (Exception e) {
+      e.printStackTrace();
+      res.setErrorResponse(503, "Oops... The Server is Down!");
+      return res;
+    }
+  }
+
+  public ServerResponse<Boolean> checkUserPass(
+    String username,
+    String password
+  ) {
+    ServerResponse<Boolean> res = new AuthResponse();
+    try {
+      String query = "?username=" + username + "&password=" + password;
+      URL url = new URI(URL + "auth" + query).toURL();
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+      conn.setRequestMethod("GET");
+      conn.setDoOutput(true);
+
+      int responseCode = conn.getResponseCode();
+      String response = "No response";
+      if (responseCode == 200) {
+        BufferedReader in = new BufferedReader(
+          new InputStreamReader(conn.getInputStream())
+        );
+        response = in.readLine();
+        in.close();
+        res.setValidResponse("");
+      } else {
+        BufferedReader in = new BufferedReader(
+          new InputStreamReader(conn.getErrorStream())
+        );
+        response = in.readLine();
+        in.close();
+        res.setErrorResponse(responseCode, response);
+      }
+      return res;
+    } catch (Exception e) {
+      e.printStackTrace();
+      res.setErrorResponse(503, "Oops... The Server is Down!");
+      return res;
+    }
+  }
+}
